d282179d7d1945010769f1efb7b77c1d32ee9f06
Marco Antonetti <63437979+marcoantonetti@users.noreply.github.com>
2023-09-12 16:00:02 -0300

    Update guessgame.js

--- guessgame.js
+++ guessgame.js
@@ -3,16 +3,33 @@ function guessgame(number){
     // User writes a number in the alert box. If it's equal to random number then you win. If not you have 4 more chances
     // If a repeated number is written down the console asks to retry the entry, without losing a chance
     // After 5 chances you lose
-    var guesses = [];  
+    var guesses = [];    
 
     for(chance = 1; chance < 6; chance++){
 
-        var guess = window.prompt("Guess a number between 0-10")
+            var guess = window.prompt("Guess a number between 0-10")
+            var intigers = ['0','1','2','3','4','5','6','7','8','9','10']
 
+
+        if(!intigers.includes(guess)){
+           
+           console.log('if runs')
+
+           window.alert("Only Numbers please")
+           chance -= 1;      
+            
+            
+        }
+
+   else {
+
+        console.log('else runs')
+
+        
         if( guess == number ){
 
                 window.alert("Congratulations")    
-                document.write(number)
+                document.write('Answer: ', number)
                 break
 
         } else if( guesses.includes(guess) ) {
@@ -35,6 +52,7 @@ function guessgame(number){
         }
 
     }
+}
 }
 
 
