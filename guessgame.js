d6db346c47bf1586875293ab15b076c8896bff18
Marco Antonetti <63437979+marcoantonetti@users.noreply.github.com>
2023-09-12 13:34:20 -0300

    Update guessgame.js

--- guessgame.js
+++ guessgame.js
@@ -1,38 +1,47 @@
 
-function guessgame(argument)
-{ // Usuario escribe un numero. Si es igual al generado por el sistema gana. Sino tiene 4 chances mas.
-    // Si ingresa numero repetido se le pide que ingreso otro de nuevo, sin perder una chance
-    // Al usar sus 5 chances, pierde
+function guessgame(number){ 
+    // User writes a number in the alert box. If it's equal to random number then you win. If not you have 4 more chances
+    // If a repeated number is written down the console asks to retry the entry, without losing a chance
+    // After 5 chances you lose
     var guesses = [];  
-for(chance=1;chance<6;chance++)
-{ 
-   var guess = window.prompt("Guess a number between 0-10")
-   if(guess==randomnumber) 
-   {
-        window.alert("Congratulations")    
-        document.write(randomnumber)
-        break
-   }
-   if(guesses.includes(guess)) {
-       window.alert("Number already inserted")
-       chance-=1
-   }
-   if(chance==5){ window.alert("Oops you lose")
-   document.write(randomnumber)}
 
-   if(guess!=randomnumber)
-   {    guesses[chance] = guess;
-       window.alert("Incorrect, remember that you have " + (5-chance) + " chances in total \n you have guessed "
-       + guesses.toString())    
-   }
+    for(chance = 1; chance < 6; chance++){
 
+        var guess = window.prompt("Guess a number between 0-10")
+
+        if( guess == number ){
+
+                window.alert("Congratulations")    
+                document.write(number)
+                break
+
+        } else if( guesses.includes(guess) ) {
+
+                window.alert("Number already inserted")
+                chance-=1
+    
+        } else if( chance == 5 ){
+
+                window.alert("Oops you lose")
+                document.write('Answer: ', number)
+                return
+
+        } else if( guess != number ){
+
+                guesses[chance] = guess;
+                window.alert("Incorrect, remember that you have " + (5-chance) + " more chances. \n You have guessed "
+                + guesses.toString())    
+
+        }
+
+    }
 }
-}
-// El programa de arriba es el juego, aca abajo pongo para que se genere numero aleatorio 
-var randomnumber = Math.round(Math.random() * 10) //esto genera un numero entero del 1-10
-            // https://medium.com/@josephcardillo/using-math-random-in-javascript-c49eff920b11
 
-// llamo a la funcion con el argumento randomnumber
-console.log(randomnumber)
+
+// Random Number Genrator
+var randomnumber = Math.round(Math.random() * 10) // https://medium.com/@josephcardillo/using-math-random-in-javascript-c49eff920b11
+
+// Calling the function
+
 guessgame(randomnumber)
 
